0d7d7fbfa14a7eca2a132e5273116e204851d88b
juangro <ing.juangro@gmail.com>
2018-10-15 17:24:37 -0500

    Call the words with their etymologies and finish the test orthography feature

--- src/pages/result-tests/result-tests.ts
+++ src/pages/result-tests/result-tests.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
-import { IonicPage, ModalController, ViewController, NavParams } from 'ionic-angular';
+import { IonicPage, ModalController, ViewController, NavParams, LoadingController, AlertController } from 'ionic-angular';
+
+// Service
+import { HttpService } from '../../services/http.service';
+
+// Model
 import { Word } from '../../models/word.model';
+import { WordComplete } from '../../models/word-complete.model';
+import { LearnNewWordsPage } from '../learn-new-words/learn-new-words';
 
 @IonicPage()
 @Component({
@@ -14,14 +21,54 @@ export class ResultTestsPage {
   constructor(
     public modalCtrl: ModalController,
     public navParams: NavParams,
-    public viewCtrl: ViewController
+    public viewCtrl: ViewController,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public httpService: HttpService
     ) {
     this.words_answered_correctly = navParams.get('words_answered_correctly');
     this.words_to_practice = navParams.get('words_to_practice');
   }
 
   private practiceWords() {
-    
+    this.httpService.postWords(this.words_to_practice, "https://etymos.herokuapp.com/complete_words")
+    .subscribe(
+      words => {
+        const loader = this.loadingCtrl.create({
+          content: "Please wait..."
+        });
+        loader.present();
+
+        let wordList: WordComplete[] = [];
+
+        if (words) {
+          for (let word of words) {
+            wordList.push(new WordComplete(word));
+          }
+          let modal = this.modalCtrl.create(LearnNewWordsPage,
+            { 
+              words: wordList
+            });
+          modal.present();
+          this.viewCtrl.dismiss();
+        } else {
+          this.showAlert('Error :(', '¡No existen palabras!', 'OK');
+        }
+        loader.dismiss();
+      },
+      error => {
+        this.showAlert('Error :(', 'Verifique su conexión a internet', 'OK');
+      }
+    );
+  }
+
+  public showAlert(title: string, msg: string, buttonMsg: string) {
+    const alert = this.alertCtrl.create({
+      title: title,
+      subTitle: msg,
+      buttons: [buttonMsg]
+    });
+    alert.present();
   }
 
   private sendToHome() {
--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
 import 'rxjs/Rx';
 
 @Injectable()
@@ -13,6 +13,21 @@ export class HttpService {
         .get(url)
         .map((response: Response) => {
             return response.json();
+        });
+  }
+
+  public postWords(wordsArray, url: string) {
+    var headers = new Headers();
+    headers.append("Accept", 'application/json');
+    headers.append('Content-Type', 'application/json' );
+    const requestOptions = new RequestOptions({ headers: headers });
+
+    let postData = { "words": wordsArray };
+
+    return this.http
+        .post(url, postData, requestOptions)
+        .map((response: Response) => {
+          return response.json();
         });
   }
 }
